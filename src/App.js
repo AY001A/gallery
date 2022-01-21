92ad8ac14eca8ba1d349a95ee720d175ef8c6fe3
AY001A <ayoola91@gmail.com>
2022-01-21 12:17:25 +0100

    gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import  {useState,useEffect} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App  ()  {
+  const [photoGalleryArray, updatePhotoGalleryArray] = useState([]);
+  useEffect(()=>{
+   
+    axios.get('https://picsum.photos/v2/list')
+  .then(function (response) {
+ 
+
+    updatePhotoGalleryArray(response.data);
+  })
+  .catch(function (error) {
+   
+    console.log(error);
+  })
+
+ },[])
+  console.log("photo array", photoGalleryArray);
+
+  return( 
+  <div className="App">
+    <nav class="navbar navbar-dark bg-dark">
+    <div className="w-100 text-light" align="center">GALLERY</div>
+    </nav>
+   
+    <div className="row">
+    {
+      photoGalleryArray.map((photoGalleryArrayItem,index)=>{
+        
+        return(
+          
+            <div key= {index} className="col-sm-4 col-md-4 col-4 p-1">
+            <img src={photoGalleryArrayItem.download_url}
+             alt={`image_${photoGalleryArrayItem.id}`}
+             height="400"
+             width="500"
+            
+             />
+          </div>
+        )
+      })
+    }
+  </div>
+  </div>
   );
-}
+};
 
 export default App;
